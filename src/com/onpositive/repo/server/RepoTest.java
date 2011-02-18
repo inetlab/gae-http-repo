e4c0adb14691c3453ae496b07889fd3c0fa25ea3
Alexander Osipov <inet.shu@gmail.com>
2011-02-18 14:54:00 +0600

    uncommented tests

--- src/com/onpositive/repo/server/RepoTest.java
+++ src/com/onpositive/repo/server/RepoTest.java
@@ -11,15 +11,15 @@ public class RepoTest {
 
 	
 	static RepoFile tests[] = {
-		//new RepoFile("http://svn.php.net/repository", "/php/php-src/branches/PHP_4_3_0/configure.in"),
-		//new RepoFile("http://code.djangoproject.com/svn", "/django/trunk/setup.py"),
-		//new RepoFile("http://redmine.rubyforge.org/svn/branches/1.1-stable", "/config/settings.yml"),
-		//new RepoFile("http://ivtvdriver.org/svn/ivtv/trunk", "/test/vbi.c"),
-		//new RepoFile("http://svn.enlightenment.org/svn/e", "/trunk/eio/src/Makefile.am"), // Trac
-		//new RepoFile("https://github.com/aav/csfr.components", "/readme.txt", RepoType.GitHub, shu, null), // GitHub /raw/master
-		//new RepoFile("https://github.com/andreyvit/groovy", "/groovy/build.xml", RepoType.GitHub, null, null), // GitHub /raw/master
-		//new RepoFile("http://trac-git.assembla.com/my-git-example", "/README", RepoType.Assembla, null, null), // Assembla GIT
-		//new RepoFile("http://lirc.cvs.sourceforge.net/viewvc/lirc", "/lirc/autogen.sh"), // CVS, SourceForge
+		new RepoFile("http://svn.php.net/repository", "/php/php-src/branches/PHP_4_3_0/configure.in"),
+		new RepoFile("http://code.djangoproject.com/svn", "/django/trunk/setup.py"),
+		new RepoFile("http://redmine.rubyforge.org/svn/branches/1.1-stable", "/config/settings.yml"),
+		new RepoFile("http://ivtvdriver.org/svn/ivtv/trunk", "/test/vbi.c"),
+		new RepoFile("http://svn.enlightenment.org/svn/e", "/trunk/eio/src/Makefile.am"), // Trac
+		new RepoFile("https://github.com/aav/csfr.components", "/readme.txt", RepoType.GitHub, shu, null), // GitHub /raw/master
+		new RepoFile("https://github.com/andreyvit/groovy", "/groovy/build.xml", RepoType.GitHub, null, null), // GitHub /raw/master
+		new RepoFile("http://trac-git.assembla.com/my-git-example", "/README", RepoType.Assembla, null, null), // Assembla GIT
+		new RepoFile("http://lirc.cvs.sourceforge.net/viewvc/lirc", "/lirc/autogen.sh"), // CVS, SourceForge
 		new RepoFile("http://pluginedge.repositoryhosting.com/trac/pluginedge_ghelpdesk", "/com.pluginedge.helpdesk/src/com/pluginedge/server/SimpleFilter.java", RepoType.Trac, pavel, "http://pluginedge.repositoryhosting.com/trac/pluginedge_ghelpdesk/login"), // Trac, Git
 
 		//URL url = new URL("http://pluginedge.repositoryhosting.com/trac/pluginedge_ghelpdesk/browser/com.pluginedge.helpdesk/src/com/pluginedge/server/SimpleFilter.java?format=txt");
