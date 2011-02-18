30d27c2d6455256476ddecab6bce2634bfa5944d
Alexander Osipov <inet.shu@gmail.com>
2011-02-18 15:10:54 +0600

    cleanup

--- src/com/onpositive/repo/server/GaeHttpRetriever.java
+++ src/com/onpositive/repo/server/GaeHttpRetriever.java
@@ -56,31 +56,7 @@ public class GaeHttpRetriever extends HttpRetriever {
 		}
 		return text;
 	}
- 
-	/*
-	public static String retrieve(URL url, Auth auth) throws IOException {
-    HTTPMethod method = HTTPMethod.GET;
-    FetchOptions fetchOptions = FetchOptions.Builder.disallowTruncate().doNotValidateCertificate();
-
-    HTTPRequest request = new HTTPRequest(url, method, fetchOptions);
-		if(auth != null) {
-			String userAndPassword = "" + auth.getUsername() + ":" + auth.getPassword();
-			String base64 = Base64.encode(userAndPassword.getBytes());
-			// workaround base64 encoder bug: http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=4615330
-			if(base64.endsWith("\n")) base64 = base64.substring(0, base64.length()-2);
-			HTTPHeader header = new HTTPHeader("Authorization", "Basic " + base64);
-			request.setHeader(header);
-		}
-    HTTPResponse response =	URLFetchServiceFactory.getURLFetchService().fetch(request);
-    if(response.getResponseCode() != 200) {
-    	System.err.println("GET error code: " + response.getResponseCode() + " URL: "+ url);
-    	return null;
-    }
-    byte[] content = response.getContent();
-		return new String(content);
-	}
-	*/
-
+ 	
 	public static void main(String[] args) throws Exception {
 		//URL url = new URL("https://github.com/aav/csfr.components/raw/master/readme.txt");
 		//Auth auth = new Auth("inetlab", "shine65");
